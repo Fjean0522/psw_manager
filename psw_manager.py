9d6e92562a7a6a2417542ad72aa9556b02a60e68
Fjean <fjean0522@gmail.com>
2024-02-10 14:58:48 -0500

    Create logic to allow users to use modes.

--- psw_manager.py
+++ psw_manager.py
@@ -18,3 +18,17 @@ def add():
         f.write(name + '|' + psw + '\n')
 
 
+# Run program in user selected mode
+while True:
+    mode = input('Would you like to add a new password or view existing ones (view, add)? type q to quit : ').lower()
+    
+    if mode == 'q':
+        break
+
+    if mode == 'view':
+        view()
+    elif mode == 'add':
+        add()
+    else:
+        print('Invalid mode selection!')
+        continue
