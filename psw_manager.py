eed46dc5ab0a3e334b8c7131c189f6cb917e4387
Fjean <fjean0522@gmail.com>
2024-02-10 14:45:53 -0500

    Create function to allow users to view passwords.

--- psw_manager.py
+++ psw_manager.py
@@ -1,5 +1,13 @@
 
 master_psw = input('What is the master password? ')
 
+# View and Add password functionality
+def view():
+    with open ('passwords.txt', 'r') as f:
+        for line in f.readlines():
+            data = line.rstrip()
+            user, psw = data.split('|')
+            print('User:', user, '| Password:', psw)
+
 
 
